c263ad21f796205b6bc956be81f2ca5e06d91b03
nelefrenn <nelson@autorun.tv>
2025-02-03 11:49:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-Entrez.email = "tuemail@gmail.com"
+Entrez.email = "nelefren@gmail.com"
 
 @app.route('/buscar', methods=['GET'])
 def buscar_articulos():
